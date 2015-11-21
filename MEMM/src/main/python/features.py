f6ccfaba3bbf1963027dc8dd48c5a7935979ae30
Yoav <yoav491@gmail.com>
2015-11-21 19:59:38 +0200

    Running demo for the minimize() method

---
+++ MEMM/src/main/python/features.py
@@ -0,0 +1,12 @@
+
+class Features(object):
+    '''
+    A class that represents the MEMM features
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
---
+++ MEMM/src/main/python/parser.py
@@ -0,0 +1,11 @@
+
+class Parser(object):
+    '''
+    A class that is in charge of the parsing of the data
+    '''
+
+    def __init__(self, path):
+        None
+#         for line in open(path,'r'):
+            
+        
