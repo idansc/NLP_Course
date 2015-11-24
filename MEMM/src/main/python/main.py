17296d122dfd86ba71f347e985905ae9a4567e51
Idan Schwartz <idanschwartz@gmail.com>
2015-11-25 01:40:59 +0200

    added parsing function, should be added to Parser (didn't work for me, not sure why)

--- MEMM/src/main/python/main.py
+++ MEMM/src/main/python/main.py
@@ -1,7 +1,26 @@
 from optimizer import Optimizer
 
+
+def get_parsed_data(filepath):
+    with open(filepath, 'r') as f:
+        word_tag_map = [("*","*")]
+        for line in f:
+#           we add start symbol
+            for wordTag in line.split(' '):
+                    word, tag = wordTag.split('_')
+                    word_tag_map.append((word,tag))
+            word_tag_map.append(("*","*"))
+    return word_tag_map;
+
+
 if __name__ == '__main__':
     print("Hello World!")
     
     y = Optimizer()
     y.optimize()
+
+    print(get_parsed_data("../resources/train.wtag"))
+
+
+
+
