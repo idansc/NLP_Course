8a2b22a677f7080683e1fc420db926ba21db7355
Yoav Nahsohn <yoavn@yahoo.com>
2015-12-08 02:07:33 +0200

    Tuning "config" to best settings yet (94.2%)

--- MEMM/src/main/python/main.py
+++ MEMM/src/main/python/main.py
@@ -57,9 +57,9 @@ if __name__ == '__main__':
         'training_data': "../resources/train.wtag",
         'test_data': "../resources/test.wtag",
         'comp_data': "../resources/comp.words",
-        'feat_threshold': 5,
+        'feat_threshold': 8,
         'viterbi_tags_treshold': 30,
-        'use_advanced_features': False,
+        'use_advanced_features': True,
         'use_common_tags': False,
         'param_vector_mode': 'load', # Options: 'stub', 'learn' or 'load'
         'learning_config': {
@@ -67,10 +67,10 @@ if __name__ == '__main__':
                 'maxiter': 16,
                 'specific_words_tag': False
             },
-        'param_vector_dump_path': '../resources/param_vector_dumps/baseline/iter16_threshold5/param_vec.dump',
+        'param_vector_dump_path': '../resources/param_vector_dumps/advanced/iter13_threshold8/param_vec.dump',
         'specific_words_tag': True,
         'confusion_matrix_print': False,
-        'competition': True
+        'competition': False
     }
     
     print("Beginning parsing...")
