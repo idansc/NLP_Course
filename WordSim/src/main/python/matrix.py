a0ed4eb2c8cbc70e9dfd7b6f37839653645127d2
Yoav Nahsohn <yoavn@yahoo.com>
2016-01-19 23:51:51 +0200

    Filtering done. Next: build PPMI matrices.

--- WordSim/src/main/python/matrix.py
+++ WordSim/src/main/python/matrix.py
@@ -3,23 +3,6 @@ from constants import BOUNDARY_SYMBOL
 from collections import Counter
 
 class TermContextMatrix(object):
-#     def __init__(self):
-#         self.contexts = []
-#         self.words = {}
-#         self.lmtzr = WordNetLemmatizer()
-#     
-#     def add_word_with_context(self, word, contexts):
-#         word = self.lmtzr.lemmatize(word.lower())
-#         contexts = [self.lmtzr.lemmatize(w.lower()) for w in contexts if w != BOUNDARY_SYMBOL]
-#         
-#         row = self.words.get(word, {})
-#         for context in contexts:
-#             if context not in self.contexts:
-#                 self.contexts.append(context)
-#             idx = self.contexts.index(context)
-#             row[idx] = row.get(idx, 0) + 1
-#         
-#         self.words[word] = row
 
     def __init__(self):
         self.contexts = Counter()
